cd8b277a9847ef16a84056f4cfcb5f60bc396bc6
RonitTsysar <tsysar.ronit@gmail.com>
2020-12-01 15:54:47 +0200

    changed read file

--- reader.py
+++ reader.py
@@ -6,10 +6,19 @@ class ReadFile:
     def __init__(self, corpus_path):
         self.corpus_path = corpus_path
 
+    def read_file(self, file_name):
+        """
+        This function is reading a parquet file contains several tweets
+        The file location is given as a string as an input to this function.
+        :param file_name: string - indicates the path to the file we wish to read.
+        :return: a dataframe contains tweets.
+        """
+        full_path = os.path.join(self.corpus_path, file_name)
+        df = pd.read_parquet(full_path, engine="pyarrow")
+        return df.values.tolist()
+
     def read_corpus(self):
         for root, dirs, files in os.walk(self.corpus_path):
             for file in files:
                 if file.endswith(".parquet"):
-                    full_path = os.path.join(self.corpus_path, os.path.join(root, file))
-                    df = pd.read_parquet(full_path, engine="pyarrow")
-                    yield df.values.tolist()
+                    yield self.read_file(os.path.join(root, file))
