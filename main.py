6a4d37d822578fd8ee0088b6bd40349be6bdbb30
RonitTsysar <tsysar.ronit@gmail.com>
2020-12-08 17:52:16 +0200

    final-partA

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import search_engine
 import utils
 
 if __name__ == '__main__':
-    # corpus_path, output_path, stemming, queries, num_docs_to_retrieve
-    '''corpus_path = 'C:\\Gal\\University\\Third_year\\semA\\InformationRetrieval\\SearchEngineProject\\Data\\Data'
+    corpus_path = 'C:\\Gal\\University\\Third_year\\semA\\InformationRetrieval\\SearchEngineProject\\Data\\Data'
     output_path = 'out'
-    stemming = False
+    stemming = True
     queries = ['Dr. Anthony Fauci wrote in a 2005 paper published in Virology Journal that hydroxychloroquine was effective in treating SARS.']
     # queries = 'queries.txt'
-    num_docs_to_retrieve = 100'''
+    num_docs_to_retrieve = 2000
 
-    # search_engine.main(corpus_path, output_path, stemming, queries, num_docs_to_retrieve)
+    search_engine.main(corpus_path, output_path, stemming, queries, num_docs_to_retrieve)
